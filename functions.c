4395d340df07ddc473e4b5a5ab8e11e3b2e77fe1
Kingjayz <tonimex2@gmail.com>
2022-11-23 14:26:08 -0800

    printf team project

--- functions.c
+++ functions.c
@@ -16,7 +16,7 @@ int print_char(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
 {
 	char c = va_arg(types, int);
-	
+
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 /************************* PRINT A STRING *************************/
@@ -35,7 +35,7 @@ int print_string(va_list types, char buffer[],
 {
 	int length = 0, i;
 	char *str = va_arg(types, char *);
-	
+
 	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
@@ -47,13 +47,13 @@ int print_string(va_list types, char buffer[],
 		if (precision >= 6)
 			str = "      ";
 	}
-	
+
 	while (str[length] != '\0')
 		length++;
-	
+
 	if (precision >= 0 && precision < length)
 		length = precision;
-	
+
 	if (width > length)
 	{
 		if (flags & F_MINUS)
@@ -71,7 +71,7 @@ int print_string(va_list types, char buffer[],
 			return (width);
 		}
 	}
-	
+
 	return (write(1, str, length));
 }
 /************************* PRINT PERCENT SIGN *************************/
@@ -114,27 +114,27 @@ int print_int(va_list types, char buffer[],
 	int is_negative = 0;
 	long int n = va_arg(types, long int);
 	unsigned long int num;
-	
+
 	n = convert_size_number(n, size);
-	
+
 	if (n == 0)
 		buffer[i--] = '0';
-	
+
 	buffer[BUFF_SIZE - 1] = '\0';
 	num = (unsigned long int)n;
-	
+
 	if (n < 0)
 	{
 		num = (unsigned long int)((-1) * n);
 		is_negative = 1;
 	}
-	
+
 	while (num > 0)
 	{
 		buffer[i--] = (num % 10) + '0';
 		num /= 10;
 	}
-	
+
 	i++;
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
@@ -156,13 +156,13 @@ int print_binary(va_list types, char buffer[],
 	unsigned int n, m, i, sum;
 	unsigned int a[32];
 	int count;
-	
+
 	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-	
+
 	n = va_arg(types, unsigned int);
 	m = 2147483648; /* (2 ^ 31) */
 	a[0] = n / m;
@@ -177,12 +177,10 @@ int print_binary(va_list types, char buffer[],
 		if (sum || i == 31)
 		{
 			char z = '0' + a[i];
-			
+
 			write(1, &z, 1);
 			count++;
 		}
 	}
 	return (count);
 }
-
-
--- functions2.c
+++ functions2.c
@@ -18,34 +18,34 @@ int print_pointer(va_list types, char buffer[],
 	unsigned long num_addrs;
 	char map_to[] = "0123456789abcdef";
 	void *addrs = va_arg(types, void *);
-	
+
 	UNUSED(width);
 	UNUSED(size);
-	
+
 	if (addrs == NULL)
 		return (write(1, "(nil)", 5));
-	
+
 	buffer[BUFF_SIZE - 1] = '\0';
 	UNUSED(precision);
-	
+
 	num_addrs = (unsigned long)addrs;
-	
+
 	while (num_addrs > 0)
 	{
 		buffer[ind--] = map_to[num_addrs % 16];
 		num_addrs /= 16;
 		length++;
 	}
-	
+
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
 		padd = '0';
 	if (flags & F_PLUS)
 		extra_c = '+', length++;
 	else if (flags & F_SPACE)
 		extra_c = ' ', length++;
-	
+
 	ind++;
-	
+
 	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
 	return (write_pointer(buffer, ind, length,
 		width, flags, padd, extra_c, padd_start));
@@ -67,27 +67,27 @@ int print_non_printable(va_list types, char buffer[],
 {
 	int i = 0, offset = 0;
 	char *str = va_arg(types, char *);
-	
+
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-	
+
 	if (str == NULL)
 		return (write(1, "(null)", 6));
-	
+
 	while (str[i] != '\0')
 	{
 		if (is_printable(str[i]))
 			buffer[i + offset] = str[i];
 		else
 			offset += append_hexa_code(str[i], buffer, i + offset);
-		
+
 		i++;
 	}
-	
+
 	buffer[i + offset] = '\0';
-	
+
 	return (write(1, buffer, i + offset));
 }
 
@@ -108,27 +108,27 @@ int print_reverse(va_list types, char buffer[],
 {
 	char *str;
 	int i, count = 0;
-	
+
 	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(size);
-	
+
 	str = va_arg(types, char *);
-	
+
 	if (str == NULL)
 	{
 		UNUSED(precision);
-		
+
 		str = ")Null(";
 	}
 	for (i = 0; str[i]; i++)
 		;
-	
+
 	for (i = i - 1; i >= 0; i--)
 	{
 		char z = str[i];
-		
+
 		write(1, &z, 1);
 		count++;
 	}
@@ -155,14 +155,14 @@ int print_rot13string(va_list types, char buffer[],
 	int count = 0;
 	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	
+
 	str = va_arg(types, char *);
 	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-	
+
 	if (str == NULL)
 		str = "(AHYY)";
 	for (i = 0; str[i]; i++)
